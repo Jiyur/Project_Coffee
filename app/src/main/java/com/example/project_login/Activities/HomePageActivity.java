49a7086a418456af15a521db8a7cb2f963d9b68b
Jiyur <53509652+Jiyur@users.noreply.github.com>
2022-04-18 21:01:49 +0700

    bug fixing

--- app/src/main/java/com/example/project_login/Activities/HomePageActivity.java
+++ app/src/main/java/com/example/project_login/Activities/HomePageActivity.java
@@ -12,5 +12,6 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+        //Mẫn
     }
 }
