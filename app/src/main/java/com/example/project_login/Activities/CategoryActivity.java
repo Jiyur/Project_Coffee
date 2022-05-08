64e829bd5245df69bdcfad969af6d73e43a7abef
ManaxIT <pmmdrg2605@gmail.com>
2022-05-08 22:21:46 +0700

    add

--- app/src/main/java/com/example/project_login/Activities/CategoryActivity.java
+++ app/src/main/java/com/example/project_login/Activities/CategoryActivity.java
@@ -1,14 +1,22 @@
 package com.example.project_login.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project_login.DTO.CategoryDTO;
 import com.example.project_login.R;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CategoryActivity extends AppCompatActivity {
+
+    ListView listView = findViewById(R.id.lstVw_Category);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        ArrayAdapter<CategoryDTO> arrayAdapter = new ArrayAdapter<CategoryDTO>(this, )
     }
 }
