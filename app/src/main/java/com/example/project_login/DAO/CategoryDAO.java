5971f4bbe915688d8d93d519848c7ca0f57bd586
ManaxIT <pmmdrg2605@gmail.com>
2022-05-08 22:03:21 +0700

    CategoryDAO

--- app/src/main/java/com/example/project_login/DAO/CategoryDAO.java
+++ app/src/main/java/com/example/project_login/DAO/CategoryDAO.java
@@ -1,4 +1,13 @@
 package com.example.project_login.DAO;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CategoryDAO {
+    private static final DatabaseReference myCategory = FirebaseDatabase
+            .getInstance("https://coffee-42174-default-rtdb.asia-southeast1.firebasedatabase.app")
+            .getReference("category");
+    public static DatabaseReference getCategory() {
+        return myCategory;
+    }
 }
