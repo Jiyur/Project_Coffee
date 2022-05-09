7647cac00ddf47b29658e2b31ee7a61ad8ffe867
ManaxIT <pmmdrg2605@gmail.com>
2022-05-09 10:25:23 +0700

    DrinkAdapter

--- app/src/main/java/com/example/project_login/Activities/DrinkActivity.java
+++ app/src/main/java/com/example/project_login/Activities/DrinkActivity.java
@@ -3,8 +3,11 @@ package com.example.project_login.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.project_login.R;
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class DrinkActivity extends AppCompatActivity {
+
+    private GridView gridView = findViewById(R.id.grdVw_Drink);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/project_login/Adapter/DrinkAdapter.java
+++ app/src/main/java/com/example/project_login/Adapter/DrinkAdapter.java
@@ -1,27 +1,81 @@
 package com.example.project_login.Adapter;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.project_login.DTO.DrinkDTO;
+import com.example.project_login.R;
+
+import java.util.List;
 
 public class DrinkAdapter extends BaseAdapter {
+    private List<DrinkDTO> drinkDTO;
+    private LayoutInflater layoutInflater;
+    private Context context;
+
+    public DrinkAdapter(Context context, List<DrinkDTO> drinkDTO) {
+        this.context = context;
+        this.drinkDTO = drinkDTO;
+        layoutInflater = LayoutInflater.from(context);
+    }
+
     @Override
     public int getCount() {
-        return 0;
+        return drinkDTO.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public Object getItem(int position) {
+        return drinkDTO.get(position);
     }
 
     @Override
-    public long getItemId(int i) {
-        return 0;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+    public View getView(int position, View view, ViewGroup viewGroup) {
+        ViewHolder holder;
+        if(view == null) {
+            view = layoutInflater.inflate(R.layout.grid_item, null);
+            holder = new ViewHolder();
+            holder.drinkImage = (ImageView) view.findViewById(R.id.imgVw_Drink);
+            holder.drinkName = (TextView) view.findViewById(R.id.txtVw_drinkId);
+            holder.drinkPrice = (TextView) view.findViewById(R.id.txtVw_drinkPrice);
+            view.setTag(holder);
+        } else {
+            holder = (ViewHolder) view.getTag();
+        }
+
+        DrinkDTO drinkDTO = this.drinkDTO.get(position);
+        holder.drinkName.setText(drinkDTO.getDrinkId());
+        holder.drinkPrice.setText(drinkDTO.getDrinkPrice());
+
+        int imageId = this.getDrawableResIdByName(drinkDTO.getDrinkImage());
+
+        holder.drinkImage.setImageResource(imageId);
+
+        return view;
+    }
+
+    public int getDrawableResIdByName(String resName) {
+        String pkgName = context.getPackageName();
+
+        int resID = context.getResources().getIdentifier(resName, "drawable", pkgName);
+        Log.i("DrinkAdapter", "Res Name: " + resName + "==> Res ID = " + resID);
+        return resID;
+    }
+
+    static class ViewHolder {
+        ImageView drinkImage;
+        TextView drinkName;
+        TextView drinkPrice;
     }
 }
