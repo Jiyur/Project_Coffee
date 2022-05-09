ebff4e66913928498e12b3b1721fcc6004f30d9e
ManaxIT <pmmdrg2605@gmail.com>
2022-05-09 10:39:55 +0700

    DrinkActivity

--- app/src/main/java/com/example/project_login/Activities/DrinkActivity.java
+++ app/src/main/java/com/example/project_login/Activities/DrinkActivity.java
@@ -1,17 +1,66 @@
 package com.example.project_login.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.project_login.Adapter.DrinkAdapter;
+import com.example.project_login.DAO.CategoryDAO;
+import com.example.project_login.DAO.DrinkDAO;
+import com.example.project_login.DTO.CategoryDTO;
+import com.example.project_login.DTO.DrinkDTO;
 import com.example.project_login.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DrinkActivity extends AppCompatActivity {
 
-    private GridView gridView = findViewById(R.id.grdVw_Drink);
+    final GridView gridView = findViewById(R.id.grdVw_Drink);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drink);
+
+        List<DrinkDTO> drinkDTOS = getListData();
+        gridView.setAdapter(new DrinkAdapter(this, drinkDTOS));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                DrinkDTO drinkDTO = (DrinkDTO) gridView.getItemAtPosition(position);
+            }
+        });
+    }
+
+    private List<DrinkDTO> getListData() {
+        List<DrinkDTO> list = new ArrayList<>();
+        DatabaseReference listDrink = DrinkDAO.getMyDrinkDatabase();
+
+        listDrink.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot: snapshot.getChildren())
+                {
+                    DrinkDTO drinkDTO = dataSnapshot.getValue(DrinkDTO.class);
+                    list.add(drinkDTO);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return list;
     }
 }
